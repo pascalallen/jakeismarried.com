c068975e739233f3a5af619e19913c0709b994b2
Pascal Allen <pascalallen@MacBook-Pro.localdomain>
2018-07-18 09:10:37 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tom and Apphia are best friends
                 </div>
 
                 <div class="links">
