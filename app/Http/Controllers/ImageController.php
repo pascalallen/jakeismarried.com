bc2c82efea3b625be89d797d72ff9cc006db3085
Pascal Allen <pascalallen@Pascals-MBP.attlocal.net>
2018-11-12 20:50:48 -0600

    Testing image upload size

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,8 +12,8 @@ class ImageController extends Controller
 {
     public function __construct()
     {
-        ini_set('post_max_size', '15M');
-        ini_set('upload_max_filesize', '15M');
+        // ini_set('post_max_size', '15M');
+        // ini_set('upload_max_filesize', '15M');
     }
     /**
      * Display a listing of the resource.
